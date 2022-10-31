a27155514e3d4a402ba277cf8ab7d6fbc73aed85
amsesk <amsesk@10-249-94-33.wireless.oregonstate.edu>
2022-10-31 10:49:21 -0700

    build release

--- shell/call_orthologs_aa_to_nucl.sh
+++ shell/call_orthologs_aa_to_nucl.sh
@@ -1,15 +1,20 @@
-BASE="/Users/aimzez/data/ehb"
-BINDIR="/Users/aimzez/dev/orthologs_aa_to_nucl"
+#!/bin/bash
+
+BASE="/Users/amsesk/DATA/bonitomes"
+BINDIR="/Users/amsesk/dev/orthologs_aa_to_nucl"
 
 GFFPATH="${BASE}/gff/"
 ASMPATH="${BASE}/asm/"
 ISOLATES_PATH="${BASE}/EHB_Isolates.xlsx"
 ORTHOTABLE_PATH="${BASE}/all_marker_orthologs_by_ltp.tsv"
 
+TARGET=release
+#TARGET=debug
+
 currdir=$(pwd)
 cd $BINDIR
-cargo build 
+cargo build --release
 cd $currdir
 
 python ~/dev/scripts/xlsx_to_tsv.py $ISOLATES_PATH | cut -f4,13 | sed 's/\t/.gff\t/' | sed "s#^#$GFFPATH#" | sed "s#\t#\t${ASMPATH}#" | \
-	~/dev/orthologs_aa_to_nucl/target/debug/orthologs_aa_to_nucl -o $ORTHOTABLE_PATH 
+	~/dev/orthologs_aa_to_nucl/target/$TARGET/orthologs_aa_to_nucl -o $ORTHOTABLE_PATH 
