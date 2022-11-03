a6eecb3b1655eb397c5b1a649e7eafdfde782095
amsesk <amsesk@10-248-17-233.wireless.oregonstate.edu>
2022-11-02 17:27:07 -0700

    trivial change

--- shell/call_orthologs_aa_to_nucl.sh
+++ shell/call_orthologs_aa_to_nucl.sh
@@ -17,4 +17,4 @@ cargo build --release
 cd $currdir
 
 python ~/dev/scripts/xlsx_to_tsv.py $ISOLATES_PATH | cut -f4,13 | sed 's/\t/.gff\t/' | sed "s#^#$GFFPATH#" | sed "s#\t#\t${ASMPATH}#" | \
-	~/dev/orthologs_aa_to_nucl/target/$TARGET/orthologs_aa_to_nucl -o $ORTHOTABLE_PATH 
+	~/dev/orthologs_aa_to_nucl/target/$TARGET/orthologs_aa_to_nucl -o $ORTHOTABLE_PATH
